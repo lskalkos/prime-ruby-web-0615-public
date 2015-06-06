f0d2afd423d5953c02881a76ebbc79ed2cdef43b
Lia Skalkos <hello@liaskalkos.net>
2015-06-06 15:31:19 -0400

    One way of doing primes

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+
+def prime?(number)
+
+  result = nil
+
+  for index in 2..number-1
+
+    if number % index == 0
+      return result = false
+    else
+      result = true
+    end
+
+  end
+
+    result
+
+end
